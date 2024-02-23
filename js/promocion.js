e461db25d0e76bb73c2eef67edda68e8a07b5275
StevenMSI <geminis_scott@hotmail.com>
2024-02-22 21:31:34 -0500

    Diseno imagen

--- js/promocion.js
+++ js/promocion.js
@@ -1,20 +1,7 @@
-const carousel = document.getElementById("carousel");
-const slides = document.querySelectorAll(".slide");
-const interval = 3000; // Intervalo de tiempo en milisegundos (3 segundos en este ejemplo)
-let currentIndex = 0;
+const $container = document.querySelector(".container");
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateCarousel();
-}
-
-function updateCarousel() {
-  const translateValue = -currentIndex * 100 + "%";
-  carousel.style.transform = "translateX(" + translateValue + ")";
-}
-
-// Espera a que todas las imágenes estén cargadas antes de iniciar el carrusel.
-window.addEventListener("load", function () {
-  updateCarousel(); // Asegurarse de que el carrusel comience desde la primera imagen.
-  setInterval(nextSlide, interval);
-});
+setInterval(() => {
+  const $last = $container.firstElementChild;
+  $last.remove();
+  $container.appendChild($last);
+}, 1000);
