579fe7fc13a33034a91360b73b983e0c25702ed6
StevenMSI <geminis_scott@hotmail.com>
2024-03-03 16:06:13 -0500

    Cambio del diseno de baner

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén la URL actual
+  var url = window.location.href;
+
+  // Obtén todos los enlaces del menú
+  var enlaces = document.querySelectorAll("nav .menu");
+
+  // Itera sobre los enlaces y compara la URL actual con la URL del enlace
+  enlaces.forEach(function (enlace) {
+    if (enlace.href === url) {
+      // Agrega una clase para resaltar la opción seleccionada
+      enlace.classList.add("seleccionado");
+    }
+  });
+});
+
+// Función para resaltar la opción seleccionada
+function seleccionarOpcion(enlace) {
+  // Elimina la clase "seleccionado" de todos los enlaces
+  var enlaces = document.querySelectorAll("nav .menu");
+  enlaces.forEach(function (e) {
+    e.classList.remove("seleccionado");
+  });
+
+  // Agrega la clase "seleccionado" solo al enlace actual
+  enlace.classList.add("seleccionado");
+}
